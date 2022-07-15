587eb54bca39fd05495cc4cac136cb6ee1f1ab69
Dominique <cyanthecomputerscientist@gmail.com>
2022-07-15 18:39:28 -0400

    Added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,37 @@
-import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
+
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">
-            <img src="../lips.png" alt="Lucious lips"></img>
-          </Link>
+          <Navbar variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>
+                  <img src="../lips.png" alt="Lucious lips"></img>
+                  CocoTi Cosmetics
+                </Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/product/:slug" element={<ProductScreen />} />
-            <Route path="/" element={<HomeScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">&copy; Cyan the Computer Scientist </div>
+        </footer>
       </div>
     </BrowserRouter>
   );
