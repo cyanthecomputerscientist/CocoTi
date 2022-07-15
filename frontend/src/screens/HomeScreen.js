8370186d2a92115ab18ea4f7159f05572433627c
Dominique <cyanthecomputerscientist@gmail.com>
2022-07-14 20:00:24 -0400

    Initial commit

---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,28 @@
+import { Link } from "react-router-dom";
+import data from "../data";
+function HomeScreen() {
+  return (
+    <div>
+      <h1>Products</h1>
+      <div className="products">
+        {data.products.map((product) => (
+          <div className="product" key={product.slug}>
+            <Link to={`/product/${product.slug}`}>
+              <img src={product.images} alt={product.name}></img>
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </Link>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+export default HomeScreen;
