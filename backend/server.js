a3d534d55fbd318429763435c4da36cc3f72de67
Dominique <cyanthecomputerscientist@gmail.com>
2022-07-29 13:57:30 -0400

    created routes and reconnected routed

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-const app = express();
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
 
+const app = express();
 dotenv.config();
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -14,28 +16,9 @@ mongoose
     console.log(error.message);
   });
 
-app.get("/api/products", (req, res) => {
-  res.send(data.products);
-});
+app.use("/api/seed", seedRouter);
 
-app.get("/api/products/slug/:slug", (req, res) => {
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product not found" });
-  }
-});
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => parseInt(req.params.id));
-
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-  console.log(req.params);
-});
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
