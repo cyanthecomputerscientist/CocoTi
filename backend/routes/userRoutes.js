7938373bf165a9ea7d7d620bcbf66784d67e63ea
Dominique <cyanthecomputerscientist@gmail.com>
2022-08-28 03:29:20 -0400

    added routes for signup

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -24,4 +24,24 @@ userRouter.post(
   })
 );
 
+userRouter.post(
+  "/signup",
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.firstName,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    const user = await newUser.save();
+    res.send({
+      _id: user._id,
+      firstName: user.firstName,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+  })
+);
+
 export default userRouter;
