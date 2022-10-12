c936138976ace16451921635893d0028615d5971
Dominique <cyanthecomputerscientist@gmail.com>
2022-10-11 22:31:19 -0400

    added email receipt functionality

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import expressAsyncHandler from "express-async-handler";
-import { isAuth, isAdmin } from "../utils.js";
+import { isAuth, isAdmin, mailgun, payOrderEmailTemplate } from "../utils.js";
 import Order from "../models/orderModel.js";
 import User from "../models/userModel.js";
 import Product from "../models/productModel.js";
@@ -118,7 +118,10 @@ orderRouter.put(
   "/:id/pay",
   isAuth,
   expressAsyncHandler(async (req, res) => {
-    const order = await Order.findById(req.params.id);
+    const order = await Order.findById(req.params.id).populate(
+      "user",
+      "email firstName lastName"
+    );
     if (order) {
       order.isPaid = true;
       order.paidAt = Date.now();
@@ -129,6 +132,24 @@ orderRouter.put(
         email_address: req.body.email_address,
       };
       const updateOrder = await order.save();
+      mailgun()
+        .messages()
+        .send(
+          {
+            from: "CocoTi Cosmetics <sales@cocoticosmetics.com>",
+            to: `${order.user.firstName} ${order.user.lastName} <${order.user.email}>`,
+            subject: `New order ${order._id}`,
+            html: payOrderEmailTemplate(order),
+          },
+          (error, body) => {
+            if (error) {
+              console.log(error);
+            } else {
+              console.log(body);
+            }
+          }
+        );
+      console.log(order);
       res.send({ message: "Order Paid", order: updateOrder });
     } else {
       res.status(404).send({ message: "Order Not Found" });
